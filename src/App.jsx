1c49f8cc3c48b4ee2105933c134f190f6e11c6a7
Jeremy Jackson <jeremy_j@ackso.net>
2018-01-29 19:54:27 -0700

    Swapi now rendering. Closes 1, 2, and 3. Continuing to put more into this just because.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
 /* eslint react/prefer-stateless-function: "off" */
+/* global fetch */
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/planets/')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
   render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: { error.message }</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        { items.map(item => (
+          <li key={item.name}>
+            {item.name} {item.rotation}
+          </li>
+        ))}
+      </ul>
     );
   }
 }
